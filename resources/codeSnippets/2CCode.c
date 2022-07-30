3e476c6290652be109c23f4cb6fd254a3885eb98
SirNico <inf19204@lehre.dhbw-stuttgart.de>
2022-07-30 16:07:09 +0200

    write

---
+++ resources/codeSnippets/2CCode.c
@@ -0,0 +1,6 @@
+if (x >= 5 && x <= 10) {
+    return x;
+}
+for (int i = 0; i < sizeof(items) / sizeof(items[0]); i++) {
+    printf(items[i]);
+}
---
+++ resources/codeSnippets/2PythonCode.py
@@ -0,0 +1,4 @@
+if 5 <= x <= 10:
+	return x
+for i in items:
+	print(i)
